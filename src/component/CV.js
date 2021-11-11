5eff07bc3e3d00fb7342bedf13756e4f2ec27e3e
mohdfazil183 <57179822+mohdfazil183@users.noreply.github.com>
2021-11-11 20:27:40 +0530

    so edit

--- src/component/CV.js
+++ src/component/CV.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navigation from './Navigation'
-import '../css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { IoPerson, IoCall,IoCalendarOutline,IoAt} from 'react-icons/io5'
 
 export default function CV() {
